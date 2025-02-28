ddbfbb12f422c35f70b244173f7ae6078606f0f5
mangeshsonone <mangeshsonone786@gmail.com>
2025-02-28 12:53:31 +0530

    Initial commit

---
+++ testapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Samaj,Family,Member
+
+admin.site.register(Samaj)
+admin.site.register(Family)
+admin.site.register(Member)
---
+++ testapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Samaj(models.Model):
+    samaj_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.samaj_name
+
+
+class Family(models.Model):
+    samaj = models.ForeignKey(Samaj, on_delete=models.CASCADE)
+    surname = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.surname
+
+
+class Member(models.Model):
+    GENDER_CHOICES = [("M", "Male"), ("F", "Female"), ("O", "Other")]
+    
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.IntegerField()
+    blood_group = models.CharField(max_length=5)
+    mobile1 = models.CharField(max_length=15)
+    mobile2 = models.CharField(max_length=15, blank=True, null=True)
